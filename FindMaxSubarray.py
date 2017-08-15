02e09457d187db96814eacbb147bc41054966a98
Andrey Dodonov <aadodonov@gmail.com>
2017-08-15 09:58:51 +0200

    finish FindMaxSubarray, Pyproj, gitignore

--- FindMaxSubarray.py
+++ FindMaxSubarray.py
@@ -1,27 +1,48 @@
-def findMaxCrossingSubarray(A,left,mid,right):
+from collections import namedtuple
+Subarray=namedtuple('Subarray', 'low high sum')
+
+def findMaxCrossingSubarray(A,low,mid,high):
     "Find subarray crossing mid, which has maximum sum of elements"
     #[left,mid]
     maxLeft=mid
     leftSum=A[mid]
     sum=A[mid]
-    for i in reversed(xrange(left,mid)):
+    for i in reversed(xrange(low,mid)):
         sum = sum + A[i]
         if sum > leftSum:
             leftSum=sum
             maxLeft = i        
     print A[maxLeft:mid+1], "leftSum:",leftSum, "maxLeft:",maxLeft
     #(mid,right]
-    maxRight=mid
-    rightSum=A[mid+1]
-    sum=A[mid+1]
-    for i in xrange(mid+2,right+1):
+    maxRight=mid+1
+    rightSum=A[maxRight]
+    sum=rightSum
+    for i in xrange(mid+2,high+1):
         sum = sum + A[i]
         if sum > rightSum:
             rightSum=sum
             maxRight = i 
     print A[mid+1:maxRight+1]," rightSum: ", rightSum, "maxRight",maxRight
-    return maxLeft,maxRight,leftSum+rightSum
-    
-A=[1,-5,3,5,7,-8]
+    return Subarray(maxLeft,maxRight,leftSum+rightSum)
 
-print(findMaxCrossingSubarray(A,0,2,5))
+def findMaxSubarray(A,low,high):
+    "Finds maximum subarray inside array A[low:high]"
+    if high == low: #base case: only one element
+        return Subarray(low,high,A[low])
+    else:
+        mid = (low+high)/2
+        left = findMaxSubarray(A,low,mid)
+        cross = findMaxCrossingSubarray(A,low,mid,high)
+        right = findMaxSubarray(A,mid+1,high)
+        if left.sum >= cross.sum and left.sum >=right.sum:
+            return left
+        elif cross.sum >= left.sum and cross.sum >= right.sum:
+            return cross
+        else:
+            return right
+
+
+A=[1,-5,1000,3,5,-100,7,-8]
+maxLeft,maxRight,sum=findMaxCrossingSubarray(A,0,2,5)
+print maxLeft,maxRight,sum
+print findMaxSubarray(A,0,5)
